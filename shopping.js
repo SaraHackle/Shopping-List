c8906af73419cb17e4a916db62eaf49d7bf92846
Sara Hackle <Shackle@ivytech.edu>
2021-11-18 13:33:44 -0600

    Initial Commit

---
+++ shopping.js
@@ -0,0 +1,43 @@
+const groceryList = [
+  {
+    id: 1,
+    name: "Milk",
+    price: 2.99,
+  },
+  {
+    id: 2,
+    name: "Eggs",
+    price: 1.99,
+  },
+  {
+    id: 3,
+    name: "Pizza Rolls",
+    price:  5.49
+  },
+  {
+    id: 4,
+    name: "Chicken",
+    price:  7.99
+  },
+  {
+    id: 5,
+    name: "Pomegranate",
+    price:  2
+  },
+  {
+    id: 6,
+    name: "Sandwich Buns",
+    price:  3.49
+  },
+  {
+    id: 7,
+    name: "Mayo",
+    price:  4.99
+  }
+  
+];
+
+console.log(groceryList)
+
+
+
